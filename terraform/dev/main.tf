3e0d82b7282222bc5eeb3b7ba347626c453d1589
Adrian caminos <camilod4568@gmail.com>
2022-09-06 00:22:58 -0500

    change type ec2

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -21,6 +21,6 @@ module "ec2" {
   fargate_memory      = "2048"
   type_instance_count = "1"
   nginx_test_name     = "dev"
-  type_instance       = "t3a.medium"
+  type_instance       = "c5a.large"
   krusty_environment  = "dev"
 }
